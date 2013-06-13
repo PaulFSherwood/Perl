be6d571d9d19d9c7d5673858a45f8528cdbaf068
paulfsherwood <paulfsherwood@gmail.com>
2013-06-12 22:53:27 -0600

    array to function updates

--- function_test.pl
+++ function_test.pl
@@ -5,20 +5,30 @@ sub printGlobals
 
     my(@original) = @{$_[0]};
     my $size = @{$_[0]};
+
     # print "$size\n\n";
     foreach my $temp (@original)
     {
-        print "$temp\n";
-        @{$_[0]}[0] = "hey";
-        # print "@{$_[0]}[0]\n";
+        if ($temp =~ /([0-9]).rpt/ig)
+        {
+            push(@{$_[1]}, $temp);
+        } else {
+            push(@{$_[2]}, $temp);
+        }
+        # @{$_[0]}[0] = "hey";
+        print "\@{\$_[0]}[1]:@{$_[0]}[1]\n";
         # shift @{$_[0]};
 
     }
 }
 
-my(@globals) = <*.pl>;
-my(@out) = ();
+my(@globals) = <doc/*.rpt>;
+my(@origComp) = ();
+my(@backComp) = ();
 
-printGlobals(\@globals);
+printGlobals(\@globals, \@origComp, \@backComp);
 
 print "@globals\n";
+print "#########################\n";
+print "@backComp\n";
+print "@origComp\n";
